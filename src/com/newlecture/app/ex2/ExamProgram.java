c7e1f38d691dacb68454b3dd0b1a97a7d8e72cf3
ilovechoo <wldms5221@icloud.com>
2023-08-22 17:50:58 +0900

    java

--- src/com/newlecture/app/ex2/ExamProgram.java
+++ src/com/newlecture/app/ex2/ExamProgram.java
@@ -1,12 +1,17 @@
 package com.newlecture.app.ex2;
 
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
 
 public class ExamProgram {
 
-	public static void main(String[] args) {
+	private static FileOutputStream fout;
+
+	public static void main(String[] args) throws IOException  {
 		int kor1, kor2, kor3;
 		int total;
 		float avg;
@@ -17,10 +22,19 @@ public class ExamProgram {
    		kor3 = 80;
    		
    		//성적 저장
-   		FileOutputStream fos = new FileOutputStream("res/data1.txt");
-//   	PrintStream fout = new PrintStream(fos, false, "UTF-8");
-		PrintWriter fout2 = new PrintWriter(fos,true, Charset.forName("UTF-8"));
-   		
+   		FileOutputStream fos = new FileOutputStream("res/exam.txt");
+   		PrintWriter fout = new PrintWriter(fos, true, Charset.forName("UTF-8"));
+   			
+//		fos.write(kor1);
+//		fos.write(kor2);
+//		fos.write(kor3);
+	
+		fout.print(kor1);
+		fout.print(kor2);
+		fout.print(kor3);
+		
+		fout.close();
+		fos.close();
    		
    		//성적 출력
 	  	total = kor1 + kor2 + kor3;
