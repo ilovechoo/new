9e72113ecdbca710b1c84e8156e0eef853f11472
ilovechoo <wldms5221@icloud.com>
2023-08-23 17:28:32 +0900

    java3

--- src/com/newlecture/app/ex2/ExamProgram.java
+++ src/com/newlecture/app/ex2/ExamProgram.java
@@ -1,11 +1,13 @@
 package com.newlecture.app.ex2;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
+import java.util.Scanner;
 
 public class ExamProgram {
 
@@ -16,34 +18,63 @@ public class ExamProgram {
 		int total;
 		float avg;
 		
-		// 성적계산
-   		kor1 = 50;
-   		kor2 = 60;	
-   		kor3 = 80;
+		// 성적입력
+   		kor1 = 0;
+   		kor2 = 0;	
+   		kor3 = 0;
+   		FileInputStream fis = new FileInputStream("res/exam.txt");
+   		Scanner fscan = new Scanner(fis);
    		
-   		//성적 저장
-   		FileOutputStream fos = new FileOutputStream("res/exam.txt");
-   		PrintWriter fout = new PrintWriter(fos, true, Charset.forName("UTF-8"));
-   			
-//		fos.write(kor1);
-//		fos.write(kor2);
-//		fos.write(kor3);
-	
-		fout.print(kor1);
-		fout.print(kor2);
-		fout.print(kor3);
-		
-		fout.close();
-		fos.close();
+//   		kor1 = fis.read();
+//   		kor2 = fis.read();
+//   		kor3 = fis.read();
+//   		kor4 = fis.read();
+//   		System.out.println(kor4);
+   		
+   		
+   		fscan.nextLine();//"국어, 영어, 수학"
+   		String line = fscan.nextLine(); //"30, 86, 100"
+   		
+   		String kor1_ = fscan.next(); //"30"
+   		kor1 = Integer.parseInt(kor1_);
+   		//System.out.println(kor1);
+   		String kor2_ = fscan.next();
+   		kor2 = Integer.parseInt(kor2_);
+   		//System.out.println(kor2);
+   		String kor3_ = fscan.next();
+   		kor3 = Integer.parseInt(kor3_);
+   		//System.out.println(kor3);
+   		
+   		fscan.close();
+   		fis.close();
+   		
+ //   		int ch = fis.read();
+//   		System.out.printf("%c\n", ch);
+//   		ch = fis.read();
+//   		System.out.printf("%c\n", ch);
+//   		ch = fis.read();
+//   		System.out.printf("%c\n", ch);
+//   		ch = fis.read();
+//   		System.out.printf("%c\n", ch);
    		
    		//성적 출력
 	  	total = kor1 + kor2 + kor3;
 	  	avg = total / 3;
 		
-      	 
-	  	System.out.printf("total is %d\n" , total);
-		System.out.printf("avg is %f\n" , avg);
-	    
+   		
+   		//성적 저장
+   		FileOutputStream fos = new FileOutputStream("res/exam-out.txt");
+   		PrintWriter fout = new PrintWriter(fos, true, Charset.forName("UTF-8"));
+		
+   		//c, s, d(x|o), f(e)
+		fout.printf("total is %d\n",total);
+		fout.printf("avg is %f\n",avg);
+		
+		fout.close();
+		fos.close();
+   		
+   		System.out.println("작업 완료");
+		
 	}
 
 }
