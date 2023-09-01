3ca9f6317360b278a2dec6c2eeefb4e13733cf8a
ilovechoo <wldms5221@icloud.com>
2023-09-01 17:14:00 +0900

    operator

---
+++ src/com/newlecture/app/ex5/operator/bit/Program.java
@@ -0,0 +1,66 @@
+package com.newlecture.app.ex5.operator.bit;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+
+public class Program {
+
+	public static void main(String[] args) throws IOException {
+		//4바이트 데이터 읽기
+		int n1;
+		int n2;
+		int n3;
+		int n4;
+		
+		
+	{
+		// FileInputStream 객체 fis를 생성, 파일명은 re/picl.bmp 
+		FileInputStream fis = new FileInputStream("res/pic1.bmp");
+		
+		//2바이트를 읽어서 버리기
+		fis.read();
+		fis.read();
+		
+		//추가로 4바이트는 정수형 변수 n1,n2,n3,n4에 저장
+		int n1 = fis.read(); //a byte of data, -1(int)
+		int n2 = fis.read(); 
+		int n3 = fis.read(); 
+		int n4 = fis.read(); 
+			 
+			
+		
+		//각 변수들을 "10,20,30,40"과 같은 포맷으로 콘솔에 출력
+		System.out.printf("%x, %x, %x, %x\n", n1, n2, n3, n4);
+		
+		fis.close();
+	}
+	
+	//크기 출력
+	{
+		// 4e,47,d,a -> 0A, 0D, 47, 4e
+//		int size = n1; //[  ][  ][  ][5e]
+						//[  ][  ][65][  ] n1<<<0
+						//[  ][24][  ][  ] n2<<8
+						//[00][  ][  ][  ] n4<<24
+		int size =  n1<<0 |
+					n2<<8 |
+					n3<<16 |
+					n4<<24;
+		
+		System.out.printf("size : %d\n", size);
+		
+	}
+	
+		
+		
+		
+		
+		
+		
+		
+		 
+
+	}
+
+}
